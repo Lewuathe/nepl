edcc80e39daddc338a32aea9bd57bce81e7ecff3
Lewuathe <sasaki_kai@lewuathe.sakura.ne.jp>
2012-11-19 20:20:10 +0900

    Create package directory

---
+++ package/NeplReader.js
@@ -0,0 +1,166 @@
+var net = require('net');
+var fs = require('fs');
+var NeplWholeMeta = require('./NeplWholeMeta');
+var NeplTXEntry = require('./NeplTXEntry.js');
+
+function NeplReader(options){
+    var self = this;
+
+    this.volume  = options.volume     ? options.volume     : __dirname + 'txvol';
+    this.meta    = options.meta       ? options.meta       : __dirname + 'meta';
+    this.name    = options.name       ? options.name       : 'nepl_reader';
+
+    if( !options.consumer || typeof options.consumer !== 'function'){
+        throw new Error('invalid consumer');
+    }
+    else{
+        this.consumer = options.consumer;
+    }
+
+   self.initLogFiles(self, this.volume, this.meta, this.name);
+}
+
+
+
+NeplReader.prototype.doConsumer = function(curr,prev){
+    var self = this;
+    var volumeName = self.wholeMeta.volumeName;
+    var currentByte = curr.size;
+    var previousByte = prev.size;
+    var writtenByte = currentByte - previousByte - 1;
+    var tx = new Buffer(writtenByte);
+    fs.open(self.volume, 'r', function(err, fd){
+        var len = fs.readSync(fd, tx, 0, writtenByte, previousByte);
+        var txEntry = new NeplTXEntry();
+        txEntry.parse(tx.toString());
+        self.consumer(txEntry);
+        self.wholeMeta.ownMetas[self.name]['lastVolTxnCnt']++;
+        var byteSum = parseInt(self.wholeMeta.ownMetas[self.name]['lastVolByteCnt']) + len;
+        self.wholeMeta.ownMetas[self.name]['lastVolByteCnt'] = byteSum + '';
+        self.wholeMeta.updateMeta(self.meta);
+    });
+}
+
+
+
+
+/* Initialization of volume file
+ *  In this function, only task is make volume file, if there is no ones.
+ *  The format of volume file obeyed to NeplTX Class.
+ */
+
+NeplReader.initVolume = function(self, volumeFd){
+    var initVolume = new Buffer('init volume\n');
+    fs.write(volumeFd, initVolume, 0, initVolume.length, 0, function(err, written, buffer){
+        if(err) throw new Error('NeplReader: Cannot initialize volume file');
+    });
+}
+
+/* Initialization of meta file
+ *  The format of meta file is below.
+ *
+ *  VolumeName=volume_name
+ *  LastTimeStamp=(new Date().getTime())
+ *  ByteOffset=(The offser of last read record)
+ *  RecordNum=(The total record count)
+ *
+ */ 
+
+NeplReader.initMeta = function(self, metaFd, name){
+    var wholeMt = self.wholeMeta;
+    var metaStr = new Buffer(wholeMt.stringBuffer() + wholeMt.ownMetaStringBuffer());
+    var metaStrLen = metaStr.length;
+    fs.write(metaFd, metaStr, 0, metaStrLen, 0, function(err, written, buffer){
+        if(err) throw new Error('NeplReader: Cannot initialize meta file');
+        self.wholeMeta.parse(self.meta);
+    });
+}
+
+
+NeplReader.prototype.initLogFiles = function(self, volume, meta, name){
+    fs.exists(volume, function(exists){
+        fs.open(volume, 'w+', function(err, fd){
+            if(err) throw new Error('NeplReader: Cannot find available volume file');
+        });
+    });
+
+    fs.exists(meta, function(exists){
+        var options = {};
+        options.volumeName = name;
+        options.metaFile = __dirname + '/meta';
+        var wholeMt = new NeplWholeMeta(options);
+        self.wholeMeta = wholeMt;
+        self.txEntry = new NeplTXEntry(options);
+
+        if(!exists){
+            // If there are no meta file, add whole meta data and own meta data.
+            fs.open(meta, 'wx', function(err, fd){
+                if(err) throw new Error('NeplReader: Cannot find available meta file');
+                // Init meta
+                NeplReader.initMeta(self, fd, name);
+                // Parse meta
+                self.wholeMeta.parse(self.meta);
+                // In this timing, wholemeta is completed
+                fs.watchFile(self.volume, function(curr, prev){
+                    self.doConsumer(curr, prev);
+                    self.wholeMeta.updateMeta(self.meta);
+                });
+            });
+        }
+        else{
+            // Only add own meta data
+            self.wholeMeta.parse(self.meta);
+            if( self.wholeMeta.ownMetas[name] ){
+                fs.watchFile(self.volume, function(curr, prev){
+                    self.doConsumer(curr, prev);
+                    self.wholeMeta.updateMeta(self.meta);
+                });
+            }
+            else{
+                fs.appendFile(meta, self.wholeMeta.ownMetaStringBuffer(), function(err){
+                    if(err) throw new Error('NeplReader: Cannot write own meta data');
+                    self.wholeMeta.parse(self.meta);
+                    // In this timing, wholemeta is completed
+                    
+                    fs.watchFile(self.volume, function(curr, prev){
+                        self.doConsumer(curr, prev);
+                        self.wholeMeta.updateMeta(self.meta);
+                    });
+                });
+            }
+        }
+
+    });
+}
+
+
+
+
+NeplReader.prototype.run = function(){
+
+}
+
+
+module.exports = NeplReader;
+
+function cons(txEntry){
+    for( var i = 0 ; i < txEntry.txs.length ; i++ ){
+        console.log('txString:', txEntry.txs[i].txString);
+    }
+}
+
+var options = {
+    ownport : 8081,
+    targetport : 8082,
+    ownhost : 'localhost',
+    targethost : 'localhost',
+    name : 'txReader',
+    volume : '/Users/sasakiumi/MyWorks/nepl/sampleTx/txvol',
+    meta   : '/Users/sasakiumi/MyWorks/nepl/sampleTx/meta',
+    consumer : cons
+};
+
+var nr = new NeplReader(options);
+
+//nr.run();
+  
---
+++ package/NeplWholeMeta.js
@@ -0,0 +1,98 @@
+var fs = require('fs');
+var NeplOwnMeta = require('./NeplOwnMeta.js');
+
+var wVOLUME_NAME        = 0;
+var wLAST_VOL           = 1;
+var wLAST_VOL_TXN_CNT   = 2;
+var wLAST_VOL_BYTE_CNT  = 3;
+var wFIRST_UNREAD_VOL   = 4;
+var wFIRST_UNREAD_TXN   = 5;
+var wOLDEST_VOL_TO_KEEP = 6;
+var oMETA_START         = 7;
+
+function NeplWholeMeta(metaObj){
+    if( arguments.length === 0 ){
+        
+    }
+    else{
+        // initialize meta file
+        if( metaObj.volumeName === undefined ){
+            throw new Error('NeplWholeMeta: Invalid meta argument');
+        }
+
+        this.metaFile = metaObj.metaFile;
+
+        var d = new Date().getTime();
+        this.volumeName    = 'meta_' + d;     // The name of this transaction
+        this.lastTimeStamp = d;   // Timestamp of last modified time
+        this.lastVol       = 0;                      // Last volume file number
+        this.lastVolTxnCnt = 0;                      // Last transaction of target log file
+        this.lastVolByteCnt = 0;                     // Last byte offset of target log file
+        this.firstUnreadVol = -1;                    // The first volume file that is unread
+        this.firstUnreadTxn = -1;                    // The first unread transaction number
+        this.oldestVolToKeep = 0;                    // The first number of kept volume file
+        
+        this.ownMetas = {};
+        this.ownMetas[metaObj.volumeName] = new NeplOwnMeta({ volumeName : metaObj.volumeName });
+    }
+}
+
+NeplWholeMeta.prototype.stringBuffer = function(){
+    var str = '';
+    str += 'volumeName=' + this.volumeName + '\n'; 
+    str += 'lastVol=' + this.lastVol + '\n';
+    str += 'lastVolTxnCnt=' + this.lastVolTxnCnt + '\n';
+    str += 'lastVolByteCnt=' + this.lastVolByteCnt + '\n';
+    str += 'firstUnreadVol=' + this.firstUnreadVol + '\n';
+    str += 'firstUnreadTxn=' + this.firstUnreadTxn + '\n';
+    str += 'oldestVolToKeep=' + this.oldestVolToKeep + '\n';
+    return new Buffer(str);
+}
+
+
+
+// Update meta file
+NeplWholeMeta.prototype.updateMeta = function(metaFile){
+    var self = this;
+    var metaStr = new Buffer(this.stringBuffer() + this.ownMetaStringBuffer());
+    var metaStrLen = metaStr.length;
+    fs.writeFileSync(metaFile, metaStr);
+    self.parse(metaFile);
+}
+
+// Return own target meta string
+NeplWholeMeta.prototype.ownMetaStringBuffer = function(){
+    var self = this;
+    var str = '';
+    for( var ownMt in self.ownMetas ){
+        str += self.ownMetas[ownMt].stringBuffer().toString();
+    }
+    return new Buffer(str);
+}
+
+// parsing and create whole meta object
+NeplWholeMeta.prototype.parse = function(metaFile){
+    var self = this;
+    var data = fs.readFileSync(metaFile);
+    var wholeMtData = data.toString().split('\n');
+    NeplWholeMeta.setParams(self, wholeMtData);
+}
+
+
+
+
+NeplWholeMeta.setParams = function(self, mtData){
+    var wholeMetaSize = mtData.length;
+    self.volumeName       = mtData[wVOLUME_NAME].split('=')[1];
+    self.lastVol          = mtData[wLAST_VOL].split('=')[1];
+    self.lastVolTxnCnt    = mtData[wLAST_VOL_TXN_CNT].split('=')[1];
+    self.lastVolByteCnt   = mtData[wLAST_VOL_BYTE_CNT].split('=')[1];
+    self.firstUnreadVol   = mtData[wFIRST_UNREAD_VOL].split('=')[1];
+    self.firstUnreadTxn   = mtData[wFIRST_UNREAD_TXN].split('=')[1];
+    self.oldestVolToKeep  = mtData[wOLDEST_VOL_TO_KEEP].split('=')[1];
+    self.ownMetas = {};
+    for( var i = oMETA_START ; i < wholeMetaSize-1 ; i++ ){
+        NeplOwnMeta.setParams(self.ownMetas, mtData[i]);
+    }
+}
+module.exports = NeplWholeMeta;
