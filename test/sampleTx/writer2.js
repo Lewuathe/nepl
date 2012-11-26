7e4154bf1758826ff589bdec2533b7c53fcea3b3
Lewuathe <sasaki_kai@lewuathe.sakura.ne.jp>
2012-11-26 21:59:27 +0900

    Remove unnecessary directory

--- test/sampleTx/writer2.js
+++
@@ -1,9 +0,0 @@
-var NeplTX = require('../src/NeplTX.js');
-
-var payload = {};
-payload['data1'] = 'suneo';
-payload['data2'] = 'dorako';
-
-var tx = new NeplTX();
-tx.setPayload(payload);
-tx.writeToVol(__dirname + '/txvol');
